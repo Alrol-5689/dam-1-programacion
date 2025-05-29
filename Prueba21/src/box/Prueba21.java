35184816d4887c01406acacd3bab276e853ea8ec
Alex <alejandrocordobarodriguez@gmail.com>
2025-05-29 12:52:21 +0200

    [general] hechas pruebas creando proyectos con java21 desde Eclipse

---
+++ Prueba21/src/box/Prueba21.java
@@ -0,0 +1,11 @@
+package box;
+
+public class Prueba21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Este programa creado desde Eclipse está en Java21 y funciona en VSC.");
+
+	}
+
+}
